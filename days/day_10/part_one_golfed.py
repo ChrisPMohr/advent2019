e11270cf7f07e9acc164d647339aa527c67c4b2c
Chris Mohr <mathmanpi@gmail.com>
2019-12-10 02:26:04 -0500

    Update part_one_golfed.py

--- days/day_10/part_one_golfed.py
+++ days/day_10/part_one_golfed.py
@@ -1,8 +1,7 @@
-from fractions import Fraction as F
+from fractions import*
 e=enumerate
 def g(a,b):
  x,y=a[0]-b[0],a[1]-b[1]
- return(F(y,x),x>0)if x!=0 else y>0
-with open('i')as f:
- s=set.union(*[{(x,y)for x,v in e(l)if v=='#'} for y,l in e(f.readlines())])
- print(sorted([(len(set(g(a,o)for o in s if o!=a)),a) for a in s],reverse=True)[0])
+ return(Fraction(y,x),x>0)if x else y>0
+s=set.union(*[{(x,y)for x,v in e(l)if v=='#'} for y,l in e(open('i').readlines())])
+print(sorted([(len(set(g(a,o)for o in s if o!=a)),a) for a in s],reverse=True)[0])
