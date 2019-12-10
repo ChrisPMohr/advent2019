3791f8f596df4648830f331666542ede99e8d31f
Chris Mohr <mathmanpi@gmail.com>
2019-12-10 02:38:01 -0500

    Update part_one_golfed.py
    -14 chars

--- days/day_10/part_one_golfed.py
+++ days/day_10/part_one_golfed.py
@@ -1,7 +1,5 @@
 from fractions import*
 e=enumerate
-def g(a,b):
- x,y=a[0]-b[0],a[1]-b[1]
- return(Fraction(y,x),x>0)if x else y>0
+g=lambda x,y: (Fraction(y,x),x>0)if x else y>0
 s=set.union(*[{(x,y)for x,v in e(l)if v=='#'} for y,l in e(open('i').readlines())])
-print(sorted((len({g(a,o)for o in s if o!=a}),a) for a in s)[-1])
+print(sorted((len({g(a[0]-o[0],a[1]-o[1])for o in s if o!=a}),a) for a in s)[-1])
