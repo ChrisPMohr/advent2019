7ef4128480e44f2a0c448ed6d3e28d660013c8d8
Chris Mohr <mathmanpi@gmail.com>
2019-12-10 02:33:57 -0500

    Update part_one_golfed.py
    Save 17 more characters

--- days/day_10/part_one_golfed.py
+++ days/day_10/part_one_golfed.py
@@ -4,4 +4,4 @@ def g(a,b):
  x,y=a[0]-b[0],a[1]-b[1]
  return(Fraction(y,x),x>0)if x else y>0
 s=set.union(*[{(x,y)for x,v in e(l)if v=='#'} for y,l in e(open('i').readlines())])
-print(sorted([(len(set(g(a,o)for o in s if o!=a)),a) for a in s],reverse=True)[0])
+print(sorted((len({g(a,o)for o in s if o!=a}),a) for a in s)[-1])
