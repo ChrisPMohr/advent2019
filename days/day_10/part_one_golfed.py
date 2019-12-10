753d687a81489704df5a3a120b160cfa83317763
Chris Mohr <mathmanpi@gmail.com>
2019-12-10 02:47:21 -0500

    Update part_one_golfed.py
    -1 char

--- days/day_10/part_one_golfed.py
+++ days/day_10/part_one_golfed.py
@@ -1,5 +1,5 @@
 from fractions import*
 e=enumerate
-g=lambda x,y: (Fraction(y,x),x>0)if x else y>0
+g=lambda x,y:(Fraction(y,x),x>0)if x else y>0
 s=set.union(*[{(x,y)for x,v in e(l)if v=='#'} for y,l in e(open('i').readlines())])
 print(sorted((len({g(a[0]-o[0],a[1]-o[1])for o in s if o!=a}),a) for a in s)[-1])
